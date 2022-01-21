025c7134bc310d8419c69f7506cffd30eaf1b25c
wei22 <weiwang24@icloud.com>
2022-01-21 14:36:02 +0800

    Initial Commit

---
+++ 密碼檢查/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  密碼檢查
+//
+//  Created by 王偉程 on 2022/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
